e063701686beb0b5aadb9bd1edd6677c6e38dfab
Felix <92216380+Felix200260@users.noreply.github.com>
2023-06-11 20:05:31 +0500

    Добавление файла

---
+++ dict.py
@@ -0,0 +1,28 @@
+
+
+
+
+human = {
+    "name": "John",
+    "age": "18",
+    "id": "188989",
+    "team": ["John", "john2", "john3"]
+}
+keys = human.keys()  # получаем ключи словаря
+
+while True:
+    input_key = input("Введите ключ (для выхода введите 'exit'): ")
+    if input_key == "exit":
+        break
+
+    found_key = False  # флаг, указывающий, был ли найден ключ "data"
+
+    for key in keys:
+        if key == input_key:
+            print("данный ключ имеется")
+            found_key = True  # устанавливаем флаг в True, если ключ найден
+            break  # выходим из цикла, если ключ найден
+
+    if not found_key:
+        print("Данного ключа не существует")  # выводим сообщение, если ключ не найден
+
